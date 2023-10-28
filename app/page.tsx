2b1b2f662dca34456a213f3fca5d1ce92a2116c4
AlthafHussainK <althafakm@gmail.com>
2023-10-28 21:53:51 +0530

    feat: UserCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
-import Image from "next/image";
 
 import { getUsers, getUserData } from "./lib/data";
 import { UserData, User } from "./lib/definitions";
+import UserCard from "./components/UserCard";
 
 export default async function Home() {
   const data: Array<User> = await getUsers();
@@ -18,28 +18,22 @@ export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between">
       <h2>List of users from GitHub</h2>
-      <div>
+      <div className="w-10/12 mt-4 flex flex-row flex-wrap justify-center">
         {richUsersData.map((user: UserData) => (
-          <div key={user.id} className="flex flex-row m-10">
-            <Image
-              src={user.avatar_url}
-              width={100}
-              height={100}
-              style={{ borderRadius: "50%", marginRight: "1em" }}
-              alt="Avatar Image"
-            />
-            {/* TODO: Refactor */}
+          <div key={user.id} className="w-2/5 m-2 ">
             <Link
               href={{
                 pathname: `/${user.login}`,
-                // query: { keyword: user.login },
               }}
             >
-              <p>
-                {user.name} ({user.login})
-              </p>
-              <p>company: {user.company}</p>
-              <p>{user.location || ""}</p>
+              <UserCard
+                avatarUrl={user.avatar_url}
+                fullname={user.name}
+                username={user.login}
+                location={user.location}
+                company={user.company}
+                bio={user.bio}
+              />
             </Link>
           </div>
         ))}
