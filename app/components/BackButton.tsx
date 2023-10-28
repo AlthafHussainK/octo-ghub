0aebe7abc7018879ea71ec7aab37a5400f449b73
AlthafHussainK <althafakm@gmail.com>
2023-10-28 21:53:31 +0530

    feat: backbutton with icon
    added octicons

--- app/components/BackButton.tsx
+++ app/components/BackButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { ArrowLeftIcon } from "@primer/octicons-react";
 
 export default function BackButton() {
   const router = useRouter();
@@ -7,5 +8,13 @@ export default function BackButton() {
     router.back();
   };
 
-  return <button onClick={handleBack}>Go Back</button>;
+  return (
+    <div
+      onClick={handleBack}
+      className={"flex space-x-2 items-center cursor-pointer"}
+    >
+      <ArrowLeftIcon size={22} />
+      <p>Back</p>
+    </div>
+  );
 }
