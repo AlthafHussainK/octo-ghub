c7f81eb04e11705807268ff1166012f805bcf134
AlthafHussainK <althafakm@gmail.com>
2023-10-29 20:48:53 +0530

    feat: global error and userCardskeleton

---
+++ app/loading.tsx
@@ -0,0 +1,35 @@
+import UserCardSkeleton from "./components/UserCardSkeleton";
+
+export default function Loading() {
+  return (
+    // <div className="flex min-h-screen flex-col items-center justify-between mt-10">
+    //   <div className=" my-4 grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 ">
+    //     <div className="my-2 md:m-2 2xl:m-3 w-80 ">
+    //       <UserCardSkeleton />
+    //     </div>
+    //     <div className="my-2 md:m-2 2xl:m-3 w-80 ">
+    //       <UserCardSkeleton />
+    //     </div>
+    //     <div className="my-2 md:m-2 2xl:m-3 w-80 ">
+    //       <UserCardSkeleton />
+    //     </div>
+    //     <div className="my-2 md:m-2 2xl:m-3 w-80 ">
+    //       <UserCardSkeleton />
+    //     </div>
+    //     <div className="my-2 md:m-2 2xl:m-3 w-80 ">
+    //       <UserCardSkeleton />
+    //     </div>
+    //     <div className="my-2 md:m-2 2xl:m-3 w-80 ">
+    //       <UserCardSkeleton />
+    //     </div>
+    //     <div className="my-2 md:m-2 2xl:m-3 w-80 ">
+    //       <UserCardSkeleton />
+    //     </div>
+    //     <div className="my-2 md:m-2 2xl:m-3 w-80 ">
+    //       <UserCardSkeleton />
+    //     </div>
+    //   </div>
+    // </div>
+    <div>Loading main page</div>
+  );
+}
