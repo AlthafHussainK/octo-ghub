6904f735419c5baa70dccb3029a6a211b2603642
AlthafHussainK <althafakm@gmail.com>
2023-10-30 19:30:26 +0530

    feat: error page

--- app/[username]/error.tsx
+++ app/[username]/error.tsx
@@ -1,5 +1,16 @@
 "use client";
 
+import Link from "next/link";
+import { AlertIcon } from "@primer/octicons-react";
+
 export default function Error() {
-  return <div>Something went wrong</div>;
+  return (
+    <div className="flex justify-center min-h-screen flex-col items-center space-y-4">
+      <AlertIcon size={72} />
+      <p className="font-semibold text-xl  ">Something went wrong</p>
+      <Link href="/">
+        <p className="text-2xl font-light">Go Home</p>
+      </Link>
+    </div>
+  );
 }
