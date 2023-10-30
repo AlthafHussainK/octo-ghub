eecc4bb9e33fdbe0e710db1ac95ffb0c3574a6ea
AlthafHussainK <althafakm@gmail.com>
2023-10-30 15:34:34 +0530

    feat: user-card unit test

--- app/components/user-card.tsx
+++ app/components/user-card.tsx
@@ -3,18 +3,16 @@ import Image from "next/image";
 
 interface UserCardProps {
   avatarUrl: string;
-  fullname: string;
+  fullname: string | null;
   username: string;
-  company: string;
-  location: string;
-  bio: string;
+  location: string | null;
 }
 
 const userCardBackground =
   "border rounded-lg drop-shadow-md bg-black/20 bg-clip-padding backdrop-filter backdrop-blur-2xl bg-opacity-80  border-gray-600";
 
 export default function UserCard(props: UserCardProps) {
-  const { avatarUrl, fullname, username, company, location, bio } = props;
+  const { avatarUrl, fullname, username, location } = props;
 
   return (
     <div className={`${userCardBackground} p-4 flex flex-row gap-x-2`}>
@@ -30,14 +28,19 @@ export default function UserCard(props: UserCardProps) {
 
       <div className="w-5/6">
         <div className="flex flex-row gap-x-1 items-end">
-          <p className="text-xl font-medium text-blue-500 truncate">
+          <p
+            data-testid="fullname"
+            className="text-xl font-medium text-blue-500 truncate"
+          >
             {fullname || username}
           </p>
           <p className="text-md text-slate-400 truncate">{username}</p>
         </div>
         <div className="flex flex-row items-center ">
           <LocationIcon size={16} />
-          <p className="text-md truncate w-2/3 ml-1">{location || "--"}</p>
+          <p data-testid="location" className="text-md truncate w-2/3 ml-1">
+            {location || "--"}
+          </p>
         </div>
       </div>
     </div>
